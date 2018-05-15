16cb90b1fa4d1e303cd2e0b05e15d79337fdf12a
Yash Jain <yashkjain1994@gmail.com>
2018-05-15 15:02:41 -0700

    Asset created by yashjain28

---
+++ code/libraries/DeviceAdministratorConstants/DeviceAdministratorConstants.js
@@ -0,0 +1,7 @@
+
+
+const PLATFORM_URL = "https://staging.clearblade.com"; // change this based on the platform instance
+const validTypes = ["String", "Int", "Bigint", "Float", "Double", "Blob", "Uuid", "Timestamp", "Boolean"];
+const  DEVICE_TYPE_COLLECTION_NAME = "DeviceTypes";
+const DEVELOPER_EMAIL = "";
+const DEVELOPER_PASSWORD = "";
